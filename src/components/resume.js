b4af6fe6006114e76784a10ab6c7d2f3a5e34a42
Amanda Barrafato <amb1047@nyu.edu>
2021-02-17 14:41:19 -0500

    Edited skills section

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,10 +1,24 @@
 import React from "react";
 import Resume from "../data/Amanda_Barrafato_Resume.pdf";
+import JSONData from "../content/skills.json";
 
 const Skills = () => (
 	<div className='resumeArea'>
 		<div className='skillsList flex'>
-			<div className='skills flex'>
+			{Object.keys(JSONData).map((data, index) => {
+				return (
+					<div className='skills flex' key={`skills_list_${index}`}>
+						<h3>{data}</h3>
+						<ul>
+							{JSONData[data].map((item, index) => {
+								return <li key={`${data}_${index}`}>{item}</li>;
+							})}
+						</ul>
+					</div>
+				);
+			})}
+
+			{/* <div className='skills flex'>
 				<h3>Frontend</h3>
 				<ul>
 					<li>JavaScript</li>
@@ -37,8 +51,9 @@ const Skills = () => (
 					<li>Pro Tools</li>
 					<li>Ableton Live</li>
 				</ul>
-			</div>
+			</div> */}
 		</div>
+
 		<a href={Resume} className='underline links'>
 			download my resume
 		</a>
