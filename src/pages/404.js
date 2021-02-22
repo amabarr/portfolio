5ffc70de985dbd2c8a6c7e90fec1562458407b3b
Amanda Barrafato <amb1047@nyu.edu>
2021-02-22 15:57:51 -0500

    Added 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,33 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import { Link } from "gatsby";
+import { Layout, SEO } from "../components";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
 const NotFoundPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+	return (
+		<Layout>
+			<SEO title='Not Found' />
+			<div className='error flex'>
+				<h1 className='error-title'>404</h1>
+				<h2 className='error'>PAGE NOT FOUND</h2>
+				<p>
+					<span role='img' aria-label='Pensive emoji'>
+						😔
+					</span>{" "}
+					Sorry{" "}
+					<span role='img' aria-label='Pensive emoji'>
+						😔
+					</span>{" "}
+					<br />
+					We couldn’t find what you were looking for.
+					<br />
+					<br />
+					<Link to='/' className='underline'>
+						Go home
+					</Link>
+				</p>
+			</div>
+		</Layout>
+	);
+};
 
-export default NotFoundPage
+export default NotFoundPage;
