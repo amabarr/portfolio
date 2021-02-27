ec1cd86746cc456dce518a7ead509491ee670743
Amanda Barrafato <amb1047@nyu.edu>
2021-02-27 16:13:39 -0500

    Added Robots plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,19 @@
+const {
+	NODE_ENV,
+	URL: NETLIFY_SITE_URL = "https://www.amanda.dev",
+	DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+	CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env;
+const isNetlifyProduction = NETLIFY_ENV === "production";
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
+
 module.exports = {
 	siteMetadata: {
 		title: "Amanda Barrafato",
 		description: "Amanda Barrafato's Portfolio",
 		author: "Amanda Barrafato",
 		image: "src/images/icon.png",
+		siteUrl,
 	},
 	plugins: [
 		"gatsby-plugin-sharp",
@@ -59,6 +69,27 @@ module.exports = {
 						},
 					},
 				],
+			},
+		},
+		{
+			resolve: "gatsby-plugin-robots-txt",
+			options: {
+				resolveEnv: () => NETLIFY_ENV,
+				env: {
+					production: {
+						policy: [{ userAgent: "*" }],
+					},
+					"branch-deploy": {
+						policy: [{ userAgent: "*", disallow: ["/"] }],
+						sitemap: null,
+						host: null,
+					},
+					"deploy-preview": {
+						policy: [{ userAgent: "*", disallow: ["/"] }],
+						sitemap: null,
+						host: null,
+					},
+				},
 			},
 		},
 	],
