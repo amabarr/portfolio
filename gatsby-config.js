8b08d75133d817a4c788d1f5cb87559063318775
Amanda Barrafato <amb1047@nyu.edu>
2021-02-27 16:31:56 -0500

    New fonts plugin setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,10 +41,22 @@ module.exports = {
 		},
 		"gatsby-plugin-dark-mode",
 		{
-			resolve: "gatsby-plugin-google-fonts",
+			resolve: "gatsby-plugin-webfonts",
 			options: {
-				fonts: [`Creepster`, "Fira Code"],
-				display: "swap",
+				fonts: {
+					google: [
+						{
+							family: "Creepster",
+							variants: ["400"],
+							text: "Amanda Barrafato",
+						},
+						{ family: "Fira Code", variants: ["400", "500", "700"] },
+					],
+				},
+				formatAgents: {
+					woff: `Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; rv:11.0) like Gecko`,
+					woff2: `Mozilla/5.0 (Windows NT 10.0; Win64; x64; ServiceUI 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393`,
+				},
 			},
 		},
 		{
