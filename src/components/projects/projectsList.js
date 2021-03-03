bbe79f3e8ed111e921d0a396edde1c78ba92de91
Amanda Barrafato <amb1047@nyu.edu>
2021-03-03 12:44:20 -0500

    Updating styling of projects

--- src/components/projects/projectsList.js
+++ src/components/projects/projectsList.js
@@ -1,28 +1,51 @@
-import React from "react";
-import JSONData from "../../content/projects.json";
+import React, { useState } from "react";
+import { projects } from "../../content/projects.json";
 import SingleProject from "./singleProject";
 import synth from "../../images/synth.jpg";
+import fft from "../../images/fft.jpg";
 import GraceCropper from "../../images/GraceCropper.jpg";
 import ExquisiteCorpse from "../../images/ExquisiteCorpse.jpg";
 
 const ProjectsList = () => {
+	const [project, setProject] = useState(0);
+
 	const image = {
+		fft: fft,
 		synth: synth,
 		GraceCropper: GraceCropper,
 		ExquisiteCorpse: ExquisiteCorpse,
 	};
 
+	const changeProject = (val) => {
+		setProject(val);
+	};
+
 	return (
 		<div className='projects-list flex'>
-			{JSONData.projects.map((data, index) => {
-				return (
-					<SingleProject
-						data={data}
-						image={image[data.image]}
-						key={`projects_list_${index}`}
-					/>
-				);
-			})}
+			<div className='project-buttons flex'>
+				{projects.map((data, index) => {
+					return (
+						<button
+							key={`project${index}`}
+							className={
+								project === index
+									? "project-selector selected"
+									: "project-selector"
+							}
+							value={index}
+							name={data.name}
+							onClick={(e) => changeProject(e.target.value)}
+						>
+							{data.name}
+						</button>
+					);
+				})}
+			</div>
+
+			<SingleProject
+				data={projects[project]}
+				image={image[projects[project].image]}
+			/>
 		</div>
 	);
 };
--- src/components/projects/singleProject.js
+++ src/components/projects/singleProject.js
@@ -6,19 +6,24 @@ export default ({ data, image }) => {
 	return (
 		<div className='project flex'>
 			<img src={image} alt={data.alt} loading='lazy' className='screenshot' />
-
 			<div className='project-des'>
 				<h3>{data.name}</h3>
+				<hr />
+				<h5>Description:</h5>
 				<p>{data.description}</p>
-			</div>
+				<h5>Overview:</h5>
+				<p>{data.overview}</p>
+				<h5>Technology used:</h5>
+				<p>{data.technology}</p>
 
-			<div className='project-links icon'>
-				<a href={data.gitHub} className='project-links-icon'>
-					<FontAwesomeIcon icon={faGithub} />
-				</a>
-				<a href={data.deployedURL} className='underline'>
-					Visit
-				</a>
+				<div className='project-links icon'>
+					<a href={data.gitHub} className='project-links-icon'>
+						<FontAwesomeIcon icon={faGithub} />
+					</a>
+					<a href={data.deployedURL} className='underline'>
+						Visit
+					</a>
+				</div>
 			</div>
 		</div>
 	);
