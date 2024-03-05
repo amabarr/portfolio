9a730e9d476aa58e7aa76a400d97c68b8f195f98
Amanda Barrafato <amb1047@nyu.edu>
2024-03-05 18:45:58 -0500

    Cleaning up skills

--- src/components/resume.tsx
+++ src/components/resume.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Resume from '../data/Amanda_Barrafato_Resume.pdf'
-import SKILLS_DATA from "../content/skills.json";
+import {SkillDataType, skillsData} from "../content/skills";
 
 export const Skills: React.FC = () => (
 	<div className='resumeArea'>
 		<div className='skillsList flex'>
-			{Object.keys(SKILLS_DATA).map((type, index) => {
+			{Object.keys(skillsData).map((type, index) => {
 				return (
 					<div className='skills flex' key={`skills_list_${index}`}>
 						<h3>{type}</h3>
 						<ul>
-							{SKILLS_DATA[type].map((item, index) => {
+							{skillsData[type as SkillDataType].map((item, index) => {
 								return <li key={`${type}_${index}`}>{item}</li>;
 							})}
 						</ul>
