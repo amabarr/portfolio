dde02577759553cf9f6e6719252d990bce16ec08
Amanda Barrafato <amb1047@nyu.edu>
2024-03-06 18:57:01 -0500

    About me updates

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -14,21 +14,18 @@ export const AboutMe = () => {
 			<div className='about'>
 				<h2>Hi, I'm Amanda</h2>
 				<span>
-					<p>
-						I am a musician, audio engineer and fullstack engineer based in
-						Brooklyn, NY.
-					</p>
+					<p>I am a musician and fullstack engineer based in Brooklyn, NY.</p>
 					<p>
 						I started playing the guitar at eight years old, and had big dreams
-						to become a heavy metal guitar goddess. I also loved to mess around
-						with computers, and electronics and found myself making a MIDI
-						guitar in college using Arduino. My ideal position is a combination
-						of my two passions in life: music and technology.
+						to become a heavy metal guitar goddess. I also loved to tinker with
+						computers / electronics, and found myself making a MIDI guitar in
+						college using Arduino. My ideal position is a combination of my two
+						passions in life: music and technology.
 					</p>
 					<p>
 						When I'm not coding, you can find me playing guitar, curled up
 						reading a book next to my cat Lucas, at a concert, playing dungeons
-						and dragons, or playing an RPG.
+						and dragons, grabbing some coffee with friends, or playing an RPG.
 					</p>
 				</span>
 
